e36d488233c68cfdc868c9b413aaa61ccc56c6c5
Craig Post <cpost@bitbuckets.org>
2020-07-26 09:17:26 -0700

    Added color wheel component

--- matches/Field.cs
+++ matches/Field.cs
@@ -3,56 +3,56 @@ using System;
 
 public class Field : StaticBody2D
 {
-    Position2D BlueBallDropSpawnPoint { get; set; }
-    Position2D RedBallDropSpawnPoint { get; set; }
+	Position2D BlueBallDropSpawnPoint { get; set; }
+	Position2D RedBallDropSpawnPoint { get; set; }
 
-    public override void _Ready()
-    {
-        var redGoal = GetNode<Area2D>("RedGoal");
-        var blueGoal = GetNode<Area2D>("BlueGoal");
-        var blueBallDrop = GetNode<Area2D>("BlueBallDrop");
-        var redBallDrop = GetNode<Area2D>("RedBallDrop");
-        BlueBallDropSpawnPoint = GetNode<Position2D>("BlueBallDrop/SpawnPoint");
-        RedBallDropSpawnPoint = GetNode<Position2D>("RedBallDrop/SpawnPoint");
+	public override void _Ready()
+	{
+		var redGoal = GetNode<Area2D>("RedGoal");
+		var blueGoal = GetNode<Area2D>("BlueGoal");
+		var blueBallDrop = GetNode<Area2D>("BlueBallDrop");
+		var redBallDrop = GetNode<Area2D>("RedBallDrop");
+		BlueBallDropSpawnPoint = GetNode<Position2D>("BlueBallDrop/SpawnPoint");
+		RedBallDropSpawnPoint = GetNode<Position2D>("RedBallDrop/SpawnPoint");
 
-        blueGoal.Connect("body_entered", this, nameof(OnBlueGoalBodyEntered));
-        redGoal.Connect("body_entered", this, nameof(OnRedGoalBodyEntered));
-        blueBallDrop.Connect("body_entered", this, nameof(OnBlueBallDropBodyEntered));
-        redBallDrop.Connect("body_entered", this, nameof(OnRedBallDropBodyEntered));
-    }
+		blueGoal.Connect("body_entered", this, nameof(OnBlueGoalBodyEntered));
+		redGoal.Connect("body_entered", this, nameof(OnRedGoalBodyEntered));
+		blueBallDrop.Connect("body_entered", this, nameof(OnBlueBallDropBodyEntered));
+		redBallDrop.Connect("body_entered", this, nameof(OnRedBallDropBodyEntered));
+	}
 
-    public void OnBlueGoalBodyEntered(Node body)
-    {
-        if (body is Ball ball && ball.ShotByPlayer && ball.Shooter.Alliance == Alliance.Blue)
-        {
-            Signals.PublishHighBallScoredEvent(ball.Shooter);
-            ball.QueueFree();
-        }
-    }
+	public void OnBlueGoalBodyEntered(Node body)
+	{
+		if (body is Ball ball && ball.ShotByPlayer && ball.Shooter.Alliance == Alliance.Blue)
+		{
+			Signals.PublishHighBallScoredEvent(ball.Shooter);
+			ball.QueueFree();
+		}
+	}
 
-    public void OnRedGoalBodyEntered(Node body)
-    {
-        if (body is Ball ball && ball.ShotByPlayer && ball.Shooter.Alliance == Alliance.Red)
-        {
-            Signals.PublishHighBallScoredEvent(ball.Shooter);
-            ball.QueueFree();
-        }
-    }
+	public void OnRedGoalBodyEntered(Node body)
+	{
+		if (body is Ball ball && ball.ShotByPlayer && ball.Shooter.Alliance == Alliance.Red)
+		{
+			Signals.PublishHighBallScoredEvent(ball.Shooter);
+			ball.QueueFree();
+		}
+	}
 
-    public void OnBlueBallDropBodyEntered(Node body)
-    {
-        if (body is Robot robot && robot.Alliance == Alliance.Blue)
-        {
-            Signals.PublishBallDropRequestedEvent(robot, BlueBallDropSpawnPoint, Vector2.Right);
-        }
-    }
+	public void OnBlueBallDropBodyEntered(Node body)
+	{
+		if (body is Robot robot && robot.Alliance == Alliance.Blue)
+		{
+			Signals.PublishBallDropRequestedEvent(robot, BlueBallDropSpawnPoint, Vector2.Right);
+		}
+	}
 
-    public void OnRedBallDropBodyEntered(Node body)
-    {
-        if (body is Robot robot && robot.Alliance == Alliance.Red)
-        {
-            Signals.PublishBallDropRequestedEvent(robot, RedBallDropSpawnPoint, Vector2.Right);
-        }
-    }
+	public void OnRedBallDropBodyEntered(Node body)
+	{
+		if (body is Robot robot && robot.Alliance == Alliance.Red)
+		{
+			Signals.PublishBallDropRequestedEvent(robot, RedBallDropSpawnPoint, Vector2.Right);
+		}
+	}
 
 }
